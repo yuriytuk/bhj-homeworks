ed0ade911f691e4b99725b0d4e1ff4ec1bf22933
yuriytuk <37401783+yuriytuk@users.noreply.github.com>
2024-09-16 17:31:36 +0300

    Update task.js
    Внесены изменения

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -1,9 +1,6 @@
 const textWindow = document.getElementById('editor');
 const deleteText = document.getElementById("clean");
 
-if (localStorage.getItem('key')) {
-    textWindow.value = localStorage.getItem('key');
-}
-
 deleteText.addEventListener('click', () => textWindow.value = '');
 window.addEventListener('unload', () => localStorage.setItem('key', textWindow.value));
+textWindow.value = localStorage.getItem('key');
