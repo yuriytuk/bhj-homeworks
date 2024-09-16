afb66f4cba518b15011440900d65bbc6f2d22fce
Yuriy <yuriytukal@yandex.ru>
2024-09-16 17:23:39 +0300

    Задание снова исправлено

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,18 +1,14 @@
 const subscribeModal = document.getElementById('subscribe-modal');
 const modalClose = subscribeModal.querySelector('.modal__close');
 
-modalClose.addEventListener('click', handler);
+subscribeModal.addEventListener('click', handler);
 
 function handler() {
-  modalClose.classList.remove('modal_active');
-  document.cookie = "modalActive=close; max-age=2629743"; //2629743
+  subscribeModal.classList.remove('modal_active');
+  document.cookie = "modalActive=close; max-age=2629743";
 }
 
-let results = document.cookie.match(/modalActive=(.+?)(;|$)/);
-try {
-  if (results[1] !== 'close') {
-    subscribeModal.classList.add('modal_active');
-  };
-} catch (error) {
-  
+let results = document.cookie;
+if (results !== 'modalActive=close') {
+  subscribeModal.classList.add('modal_active');
 }
